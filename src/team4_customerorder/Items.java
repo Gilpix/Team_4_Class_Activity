7bed433af3713c95fabc5fea53439fdcd0856845
Parwinder <sunnyjasti15@gmail.com>
2019-02-10 09:24:58 -0500

    Items Class created with variables and constructor

---
+++ src/team4_customerorder/Items.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team4_customerorder;
+
+/**
+ *
+ * @author temp
+ */
+public class Items {
+    private int shippingWeight;
+    private String shippingDesc;
+    
+    public Items(int shipWeight, String shipDesc)
+    {
+        shippingWeight =shipWeight;
+        shippingDesc=shipDesc;
+    }
+    
+}
